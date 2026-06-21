1fd4608e88a94f0f9da8c2b6846b1d4bdfc7f198
Takuhooo <takuho0104@ezweb.ne.jp>
2026-06-21 11:47:44 +0900

    ログイン機能でも詳細に分けてブランチ作成

--- Main.java
+++ Main.java
@@ -6,5 +6,12 @@ public class Main {
         System.err.println("git addA");
         System.err.println("git addB");
         System.err.println("git addC");
+
+        System.out.println("ログイン機能でも下記のようにブランチを分けて作業
+                    ログイン画面作成feature/loguin\n" + //
+                    "認証処理作成feature/auth\n" + //
+                    "DB接続\n" + //
+                    "バリデーション\n" + //
+                    "エラーメッセージ");
     }
 }
